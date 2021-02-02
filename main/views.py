738cee1803bc08264df4c7d6fe72291106b745a9
Alan Pedraza <alanbanano@gmail.com>
2021-02-01 20:25:16 -0600

    Django Forms Full Working - CRUD COMPLETE

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django import views
 from .models import UserModel
+from .forms import UserForm
 from django.contrib import messages
 
 class GetUsersView (views.View):
@@ -25,11 +26,17 @@ def GetUserView(request, id):
 
 class CreateUserView(views.View):
     template_name = 'main/form.html'
+    action = 'create'
     def get(self, request):
-        return render(request, self.template_name)
+        form = UserForm()
+        context = {
+            'form': form,
+            'action': self.action
+        }
+        return render(request, self.template_name, context)
 
     def post(self, request):
-        first_name = request.POST['first_name']
+        """ first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
         phone_number = request.POST['phone_number']
@@ -53,29 +60,64 @@ class CreateUserView(views.View):
             return redirect('/main')
         else:
             messages.error(request, 'Algo Fallo al momento de crear un usuario')
-            return render(request, self.template_name)
+            return render(request, self.template_name) """
+
+        new_form = UserForm(request.POST)
+        if new_form.is_valid():
+            form_data = new_form.save(commit=False)
+            form_data.save()
+            return redirect('/main')
+        else:
+            errors = new_form.errors.as_data()
+            print(errors)
+            form = UserForm()
+            context = {
+                'form': form,
+                'action': self.action
+            }
+            return render(request, self.template_name, context)
+
 
 class UpdateUserView(views.View):
     template_name = 'main/form.html'
+    action = 'update'
     def get(self, request, id):
         user = UserModel.objects.get(id=id)
-        print(user)
+        form = UserForm(instance=user)
         context = {
-            'user': user
+            'user': user,
+            'action': self.action,
+            'form': form
         }
         return render(request, self.template_name, context)
 
     def post(self, request, id):
         user = UserModel.objects.get(id=id)
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.phone_number = request.POST['phone_number']
-        user.gender = request.POST['gender']
-        user.date_birth = request.POST['date_birth']
-        user.save()
-        messages.success(request, 'Usuario Actualizado Exitosamente!')
-        return redirect('/main/' + str(id))
+        # user.first_name = request.POST['first_name']
+        # user.last_name = request.POST['last_name']
+        # user.email = request.POST['email']
+        # user.phone_number = request.POST['phone_number']
+        # user.gender = request.POST['gender']
+        # user.date_birth = request.POST['date_birth']
+        # user.save()
+        # messages.success(request, 'Usuario Actualizado Exitosamente!')
+        # return redirect('/main/' + str(id))
+        edit_form =UserForm(request.POST, instance=user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return redirect('/main/' +  str(id))
+        else:
+            errors = edit_form.errors.as_data()
+            print(errors)
+            form = UserForm(instance=user)
+            context = {
+                'form': form,
+                'action': self.action,
+                'user': user
+            }
+            return render(request, self.template_name, context)
+
+
 
 def DeleteUserView(request, id):
     user = UserModel.objects.get(id=id)
