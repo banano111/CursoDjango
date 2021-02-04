36b356cee43215234b159e7b00bd9cff9999a4e8
Alan Pedraza <alanbanano@gmail.com>
2021-02-04 14:03:45 -0600

    Created Profile Model - Migration Done

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ GENDER_OPTS = (
     ('other', 'Otros')
 )
 
-class UserModel (models.Model):
+class UserModel(models.Model):
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -19,3 +19,15 @@ class UserModel (models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+class Profile(models.Model):
+    
+    user = models.OneToOneField(UserModel, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='users/profiles/photos', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    is_public = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
